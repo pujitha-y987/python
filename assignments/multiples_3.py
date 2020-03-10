0734e9f4c56aa6b979d3f3d61c7419ff03dbcbbb
pujitha-y987 <61104456+pujitha-y987@users.noreply.github.com>
2020-03-10 15:32:52 +0530

    Update multiples_3.py

--- assignments/multiples_3.py
+++ assignments/multiples_3.py
@@ -8,7 +8,7 @@ Created on Wed Feb 26 19:35:15 2020
 # Print all 3 multiples from 1 to 100 using for loop.
 
 
-a=[i*3 for i in range(1,101)]
+a=[i for i in range(1,101) if i%3==0]
 print(list(a))
 
 
