dfdaf4709f50fc350848be48cceb1642209e89b3
pujitha-y987 <61104456+pujitha-y987@users.noreply.github.com>
2020-05-27 16:25:31 +0530

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -87,7 +87,15 @@ for i in range(1000,3001):
         print(s,end=" ")
  
 
+# 99  Write a program that accepts a sequence of whitespace separated words as input and prints the words after removing all duplicate words and sorting them alphanumerically. Suppose the following input is supplied to the program: hello world and practice makes perfect and hello world again Then, the output should be: again and hello makes perfect practice world
  
+l=input().split()
+l1=sorted(tuple(set(l)))
+print(" ".join(l1))     
+ 
+         (OR)
+
+print(" ".join(sorted(tuple(set(input().split())))))    
  
  
  
