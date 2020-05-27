511b2ae8118c12ca2a212c0beeb381525061a2f1
pujitha-y987 <61104456+pujitha-y987@users.noreply.github.com>
2020-05-27 16:14:55 +0530

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -76,6 +76,17 @@ print("compressed string\n  ",s)
 t = gzip.decompress(s) 
 print("\ndecompressed string  \n",t) 
 
+
+
+#107 Write a program, which will find all such numbers between 1000 and 3000 (both included) such that each digit of the number is an even number. The numbers obtained should be printed in a comma-separated sequence on a single line.
+ 
+for i in range(1000,3001):
+    s=str(i)
+    
+    if (int(s[0])%2==0 and int(s[1])%2==0 and int(s[2])%2==0 and int(s[3])%2==0):
+        print(s,end=" ")
+ 
+
  
  
  
