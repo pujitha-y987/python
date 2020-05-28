1438de633fc9c672c29476e0bedaaa4e423de100
pujitha-y987 <61104456+pujitha-y987@users.noreply.github.com>
2020-05-28 11:36:42 +0530

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -103,7 +103,12 @@ print(" ".join(sorted(tuple(set(input().split())))))
 import random
 print(random.randint(7,16)) 
  
+# 22 write a program to output a random even number between 0 and 10 inclusive using random module and list comprehension.
+
  
+ random.sample([i for i in range(0,11,2)],1)
+ 
+  
  
  
  
