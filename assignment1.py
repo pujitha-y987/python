09f0cbf806a651ff0ea2f2567ad8179728047fd7
pujitha-y987 <61104456+pujitha-y987@users.noreply.github.com>
2020-06-01 17:39:40 +0530

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -163,6 +163,11 @@ def square():
   print(l[5:])  
 square() 
  
+# 28 Please write a program which accepts basic mathematic expression from console and print the evaluation result. Example: If the following string is given as input to the program: 35+3 Then, the output of the program should be: 38
+from math import *
+s1=input()
+y = eval(s1) 
+print(y)  
   
  
  
