016a96b022f8a92abad2bf05d22697694dfd0ab0
pujitha-y987 <61104456+pujitha-y987@users.noreply.github.com>
2020-06-01 15:55:09 +0530

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -116,7 +116,25 @@ def string(a,b):
 string(input(),input())
  
  
+# 1 write a program to find the GCD or HCF of set of numbers 
+def hcf(x, y):  
+   if x > y:  
+       smaller = y  
+   else:  
+       smaller = x  
+   for i in range(1,smaller + 1):  
+       if((x % i == 0) and (y % i == 0)):  
+           hcf = i  
+   return hcf  
+  
+num1 = int(input("Enter first number: "))  
+num2 = int(input("Enter second number: "))  
+print("The H.C.F. of", num1,"and", num2,"is", hcf(num1, num2))   
  
+ 
+ 
+ 
+  
  
  
  
